e7b96478d532a939fa472f2971f4ff59e04cec81
Nick Torres <nickrtorres@icloud.com>
2020-12-27 11:46:10 -0800

    lex each line then pass it to parse

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,19 +7,17 @@ use bandera::Vm;
 
 fn repl() {
     let stdin = stdin();
-    let mut handle = stdin.lock();
+    let ops = parse(
+        stdin
+            .lock()
+            .lines()
+            .filter(Result::is_ok)
+            .flat_map(|line| lex(line.unwrap().chars().peekable()))
+            .peekable(),
+    );
 
     let mut vm = Vm::new();
-    loop {
-        let mut buffer = String::with_capacity(1024);
-        if handle.read_line(&mut buffer).expect("could not read line") == 0 {
-            break;
-        }
-
-        let tokens = lex(buffer.chars().peekable());
-        let ops = parse(tokens.into_iter().peekable());
-        vm.run(ops).expect("uh-oh!");
-    }
+    vm.run(ops).expect("uh-oh!");
 }
 
 fn main() {
